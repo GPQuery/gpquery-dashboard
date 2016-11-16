786bfc9f54d3f4605a0d0abb0129a246ae6cbfd9
Joho <joho@joho.io>
2016-11-15 19:02:54 -0500

    Create Dashboard module pages & fix styles

--- client/app/dashboard/dashboard.module.js
+++ client/app/dashboard/dashboard.module.js
@@ -6,6 +6,30 @@
 
       'gpquery.dashboard.main'
 
-    ]);
+    ])
+    .config(routerConfig);
+
+  /** ngInject */
+  function routerConfig($stateProvider) {
+
+    $stateProvider
+      .state('overview', {
+        url:          '/overview',
+        templateUrl:  'app/dashboard/dashboard.html',
+        controller:   'DashboardController',
+        controllerAs: 'dash',
+        resolve: {
+          $title: function() {
+            return 'Overview';
+          },
+          $bodyClass: function() {
+            return 'dash-page overview';
+          },
+          data: function(webDevTec) {
+            return webDevTec.getTec();
+          }
+        }
+      });
+  }
 
 })();
